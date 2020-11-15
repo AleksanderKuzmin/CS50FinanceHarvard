a96ba8b7c6ec5099b59f825fabe9b22998d63d57
Alex K <akzmn@MacBook.local>
2020-11-15 15:02:25 -0500

    first test modifited

--- tests/login.py
+++ tests/login.py
@@ -8,14 +8,13 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path='../browser_drivers/geckodriver')
         self.browser.get('http://127.0.0.1:5000/login')
+        self.addCleanup(self.browser.quit)
 
     def test_login_page_exist(self):
         browser = self.browser
         sleep(4)
         login_text = browser.find_element_by_xpath('//*[@id="navbar"]/ul/li[2]/a').text
         self.assertEqual('Log In', login_text)
-
-        browser.quit()
 
 
 if __name__ == '__main__':
